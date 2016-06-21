bb319c005642011087e31a8069c273605b5ca419
udeved <artoo@manjaro.org>
2016-06-21 19:07:07 +0200

    Initial commit

---
+++ Makefile
@@ -0,0 +1,53 @@
+Version=16.09
+
+PREFIX = /usr/local
+
+SCRIPTS = \
+	scripts/keyring-upgrade \
+	scripts/dev-keys-upgrade
+
+HOOKS = \
+	hooks/keyring-upgrade.hook \
+	hooks/dev-keys-upgrade.hook
+
+LIB = \
+	lib/util-pacman.sh
+
+all: $(SCRIPTS)
+
+edit = sed -e "s|@prefix@|${PREFIX}|"
+
+%: %.in Makefile
+	@echo "GEN $@"
+	@$(RM) "$@"
+	@m4 -P $@.in | $(edit) >$@
+	@chmod a-w "$@"
+	@chmod +x "$@"
+
+clean:
+	rm -f $(SCRIPTS)
+
+install:
+	install -dm0755 $(DESTDIR)$(PREFIX)/share/libalpm/scripts
+	install -m0755 ${SCRIPTS} $(DESTDIR)$(PREFIX)/share/libalpm/scripts
+
+	install -dm0755 $(DESTDIR)$(PREFIX)/share/libalpm/hooks
+	install -m0644 ${HOOKS} $(DESTDIR)$(PREFIX)/share/libalpm/hooks
+
+	install -dm0755 $(DESTDIR)$(PREFIX)/share/libalpm
+	install -m0644 ${LIB} $(DESTDIR)$(PREFIX)/share/libalpm
+
+uninstall:
+	for f in ${SCRIPTS}; do rm -f $(DESTDIR)$(PREFIX)/share/libalpm/scripts/$$f; done
+	for f in ${HOOKS}; do rm -f $(DESTDIR)$(PREFIX)/share/libalpm/hooks/$$f; done
+	for f in ${LIB}; do rm -f $(DESTDIR)$(PREFIX)/share/libalpm/$$f; done
+
+install: install
+
+uninstall: uninstall
+
+dist:
+	git archive --format=tar --prefix=manjaro-system-ng-$(Version)/ $(Version) | gzip -9 > manjaro-system-ng-$(Version).tar.gz
+	gpg --detach-sign --use-agent manjaro-system-ng-$(Version).tar.gz
+
+.PHONY: all clean install uninstall dist
---
+++ lib/util-pacman.sh
@@ -0,0 +1,103 @@
+#!/bin/sh
+
+if [[ -d /run/systemd ]];then
+	is_systemd=true
+else
+	is_systemd=false
+fi
+
+
+rm_db_lck(){
+	rm /var/lib/pacman/db.lck &> /dev/null
+}
+
+mk_db_lck(){
+	touch /var/lib/pacman/db.lck &> /dev/null
+}
+
+is_symlink(){
+	if [ -L "$1" ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+is_installed(){
+	if pacman -Qq $1; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+install_pkg(){
+	local pkg=$1
+	rm_db_lck
+	pacman --noconfirm -S $pkg
+	mk_db_lck
+}
+
+run_pac_cmd(){
+	local args=$1 pkgs=$2
+	rm_db_lck
+	pacman --noconfirm -$args $pkgs
+	mk_db_lck
+}
+
+remove_pkg(){
+	local pkg=$1
+	rm_db_lck
+	pacman --noconfirm -Rdd $pkg
+	mk_db_lck
+}
+
+ver_is_greater(){
+	if [ "$(vercmp $1 $2)" -gt 0 ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+ver_is_lower(){
+	if [ "$(vercmp $1 $2)" -lt 0 ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+ver_is_equal(){
+	if [ "$(vercmp $1 $2)" -eq 0 ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
+get_pkg_ver(){
+	local pkg=$1 result
+	result=$(pacman -Q $pkg)
+	echo ${result##* }
+}
+
+set_pkg_ver(){
+    ver=$(get_pkg_ver $1)
+}
+
+err() {
+	ALL_OFF="\e[1;0m"
+	BOLD="\e[1;1m"
+	RED="${BOLD}\e[1;31m"
+	local mesg=$1; shift
+	printf "${RED}==>${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@" >&2
+}
+
+msg() {
+	ALL_OFF="\e[1;0m"
+	BOLD="\e[1;1m"
+	GREEN="${BOLD}\e[1;32m"
+	local mesg=$1; shift
+	printf "${GREEN}==>${ALL_OFF}${BOLD} ${mesg}${ALL_OFF}\n" "$@" >&2
+}
